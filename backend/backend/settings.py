b1ba03c13b6aeee21da24451e762f8511c81f574
Deepak Poudel <deepak.poudel077@gmail.com>
2026-06-25 19:43:54 +0545

    runner chagned from ubuntu-latest to self-hosted

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -28,7 +28,7 @@ if not os.environ.get('KUBERNETES_SERVICE_HOST'):
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+ = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
@@ -94,8 +94,6 @@ DATABASES = {
         "PORT": os.environ.get("DB_PORT", "5432"),
     }
 }
-
-print(os.environ.get('POSTGRES_PASSWORD'))
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
